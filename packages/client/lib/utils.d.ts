7e6c8094e5af3d75e51515575fa591347d8fb64b
Martin Becze <martin@becze.org>
2024-06-09 11:15:07 +0200

    remove compiled files

--- packages/client/lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function requestId(): Uint8Array;
-export declare function eventId(): Uint8Array;
-export declare function randomBytes(n: number): Uint8Array;
-export declare function convertFirstCharToLowerCase(str: string): string;
-export declare function snakeToCamel(str: string): string;
-export declare function formatMessageForSigning(obj: Record<string, Uint8Array | string | number | Uint8Array[] | number[]>): Record<string, string | `0x${string}`[] | BigInt | BigInt[]>;
-export declare function getRandomStoreId(): `0x${string}`;
-export declare function generatePk(): `0x${string}`;
-export declare function hexToBase64(hex: string): string;
