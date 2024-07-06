6b576256284c3b88223a32ebd1ab6950e24bf544
Henry <cryptix@riseup.net>
2024-07-06 13:20:52 +0200

    prettier

--- packages/frontend/src/app/add-user/page.tsx
+++ packages/frontend/src/app/add-user/page.tsx
@@ -21,38 +21,34 @@ import { useStoreContext } from "@/context/StoreContext";
 import { BlockchainClient } from "@massmarket/blockchain";
 
 const StoreCreation = () => {
-  const {
-    clientWallet,
-    shopId,
-  } = useMyContext();
+  const { clientWallet, shopId } = useMyContext();
 
   const [addUserAddr, setAddUserAddr] = useState<string>("");
-  
 
   const { isConnected, setIsConnected, setIsMerchantView } = useAuth();
   const chains = useChains();
   const router = useRouter();
 
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
- 
 
   const runAddUser = async () => {
-    if  (clientWallet && addUserAddr !== "") {
+    if (clientWallet && addUserAddr !== "") {
       const blockchainClient = new BlockchainClient(shopId);
       const walletAddr = addUserAddr as `0x${string}`;
-      const result = await blockchainClient.addAdminToShop(clientWallet, walletAddr);
-      console.log("user added:")
-      console.log(result)
+      const result = await blockchainClient.addAdminToShop(
+        clientWallet,
+        walletAddr,
+      );
+      console.log("user added:");
+      console.log(result);
     }
-  }
+  };
 
   return (
     <main className="pt-under-nav h-screen p-4 mt-5">
       {errorMsg ? <p>{errorMsg}</p> : null}
       <div className="flex">
-
-
-      <div className="m-4">
+        <div className="m-4">
           <p className="font-sans">General</p>
           <section className="text-sm flex flex-col gap-4">
             <div>
@@ -70,14 +66,14 @@ const StoreCreation = () => {
                     onChange={(e) => setAddUserAddr(e.target.value)}
                   />
                   <div className="mt-auto mx-4">
-                   <Button onClick={runAddUser}>Add</Button>
+                    <Button onClick={runAddUser}>Add</Button>
                   </div>
                 </form>
               </section>
             </div>
           </section>
         </div>
-        </div>
+      </div>
     </main>
   );
 };
