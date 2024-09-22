44afbfdc6184617fb985a32a38439364af395944
Jinyoung <jinyoung.choi@mass.market>
2024-09-22 10:47:03 -0700

    genericResponse to client

--- packages/client/src/stream.ts
+++ packages/client/src/stream.ts
@@ -47,8 +47,10 @@ export class ReadableEventStream {
             self.controller.enqueue({ event, signer });
           }
           // Send a response to the relay to indicate that we have processed the events
-          self.client.encodeAndSendNoWait(schema.EventPushResponse, {
+          self.client.encodeAndSendNoWait({
             requestId,
+            //FIXME : what should be the payload in this response we are sending to client?
+            response: {},
           });
         }
         await self.nextPushReq;
@@ -58,12 +60,14 @@ export class ReadableEventStream {
   }
 
   // This method is meant to be used by the client to enqueue events into the stream
-  enqueue(pushReq: schema.EventPushRequest) {
+  enqueue(pushReq: schema.SubscriptionPushRequest) {
     this.queue.push(pushReq);
     this.resolve(null);
-    this.nextPushReq = new Promise<schema.EventPushRequest>((resolve) => {
-      this.resolve = resolve;
-    });
+    this.nextPushReq = new Promise<schema.SubscriptionPushRequest>(
+      (resolve) => {
+        this.resolve = resolve;
+      },
+    );
   }
   //This method enqueues events created by the client, since these are not sent back from the relay.
   async outgoingEnqueue(event: schema.IShopEvent) {
