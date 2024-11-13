6d3341ecfd057244668f2269250f34e62972a7b0
Henry <cryptix@riseup.net>
2024-11-13 18:29:55 +0700

    add OP Sepolia for testing

--- packages/frontend/src/wagmi.ts
+++ packages/frontend/src/wagmi.ts
@@ -1,16 +1,23 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { hardhat, mainnet, optimism, sepolia } from "wagmi/chains";
+import {
+  hardhat,
+  mainnet,
+  optimism,
+  optimismSepolia,
+  sepolia,
+} from "wagmi/chains";
 import { http } from "wagmi";
 
 export const config = getDefaultConfig({
   appName: "Mass Labs",
   projectId: "6c432edcd930e0fa2c87a8d940ae5b91",
-  chains: [mainnet, optimism, sepolia, hardhat],
+  chains: [mainnet, optimism, sepolia, hardhat, optimismSepolia],
   ssr: false,
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
     [hardhat.id]: http(),
     [optimism.id]: http(),
+    [optimismSepolia.id]: http(),
   },
 });
