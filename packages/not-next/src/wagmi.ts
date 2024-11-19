919df5795ab4dcf517d408ff8ac532e312f8908b
Martin Becze <martin@becze.org>
2024-11-19 11:45:10 +0000

    Stating a not next frontend

---
+++ packages/not-next/src/wagmi.ts
@@ -0,0 +1,16 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { hardhat, mainnet, optimism, sepolia } from "wagmi/chains";
+import { http } from "wagmi";
+
+export const config = getDefaultConfig({
+  appName: "Mass Labs",
+  projectId: "6c432edcd930e0fa2c87a8d940ae5b91",
+  chains: [mainnet, optimism, sepolia, hardhat],
+  ssr: false,
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [hardhat.id]: http(),
+    [optimism.id]: http(),
+  },
+});
