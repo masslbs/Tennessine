77901b1541a33dcdb9515e04bcc816407bfe48a1
Martin Becze <martin@becze.org>
2024-11-19 12:49:32 +0000

    css fix

--- packages/not-next/test/happyDomSetup.ts
+++ packages/not-next/test/happyDomSetup.ts
@@ -2,5 +2,5 @@
 import { GlobalRegistrator } from "npm:@happy-dom/global-registrator";
 GlobalRegistrator.register();
 // create a new window, base url doesn't matter
-// const window = new Window({ url: "https://localhost:8080" });
-// globalThis.window = window;
+// @ts-ignore: globalThis.window is from deno, and we are overwriting it one that comes from happy-dom
+globalThis.window = new Window({ url: "https://localhost:877" });
