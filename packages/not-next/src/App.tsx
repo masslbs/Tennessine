e7d2df45601910ffd0af8d0995fdecdf9afcbf31
Martin Becze <martin@becze.org>
2024-11-19 12:49:32 +0000

    css fix

--- packages/not-next/src/App.tsx
+++ packages/not-next/src/App.tsx
@@ -1,9 +1,7 @@
-// import "./App.css"
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import "@rainbow-me/rainbowkit/styles.css";
 import { config } from "./wagmi.ts";
 const queryClient = new QueryClient();
 
