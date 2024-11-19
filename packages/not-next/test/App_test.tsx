2c57c13703598c872a9dae22a8d9b6af2bc46668
Martin Becze <martin@becze.org>
2024-11-19 16:29:35 +0000

    use inject provide as the first prefance

--- packages/not-next/test/App_test.tsx
+++ packages/not-next/test/App_test.tsx
@@ -1,9 +1,12 @@
 import "./happyDomSetup.ts";
 import { assertEquals } from "jsr:@std/assert";
 import { render, screen } from "npm:@testing-library/react";
-import App from "../src/App.tsx";
 
-Deno.test("check that we can render the app", () => {
+Deno.test("check that we can render the app", async () => {
+  // we need to import App.tsx here since wagmi and rainbowkit setup timers
+  const {
+    default: App,
+  } = await import("../src/App.tsx");
   render(<App />);
   const hello = screen.getByTestId("hello");
   assertEquals(hello.textContent, "hello!");
