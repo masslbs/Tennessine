e064dea5a7f7ee8e249e382bd1f4829d44a463be
JC <70621556+youngbbcoconut@users.noreply.github.com>
2024-11-21 18:40:05 +0900

    custom hooks (#158)

---
+++ packages/not-next/src/utils/chain.ts
@@ -0,0 +1,28 @@
+import { hardhat, mainnet, sepolia } from "viem/chains";
+import { createPublicClient, http } from "viem";
+
+const defaultRPC =
+  (process && process.env && process.env.NEXT_PUBLIC_ETH_RPC_URL) ||
+  "http://localhost:8545";
+
+export function getUsedChain() {
+  const chainName = process.env.NEXT_PUBLIC_CHAIN_NAME!;
+  switch (chainName) {
+    case "hardhat":
+      return hardhat;
+    case "sepolia":
+      return sepolia;
+    case "mainnet":
+      return mainnet;
+    default:
+      throw new Error(`unhandled chain name ${chainName}`);
+  }
+}
+
+export function createPublicClientForChain() {
+  const chain = getUsedChain();
+  return createPublicClient({
+    chain,
+    transport: http(defaultRPC),
+  });
+}
