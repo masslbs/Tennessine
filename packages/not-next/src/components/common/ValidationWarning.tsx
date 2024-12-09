5d2e433b5456e766645b207beaf52b445c8e19eb
Jinyoung <jinyoung.choi@mass.market>
2024-12-09 14:48:04 +0900

    wip

--- packages/not-next/src/components/common/ValidationWarning.tsx
+++ packages/not-next/src/components/common/ValidationWarning.tsx
@@ -1,32 +1,32 @@
-// SPDX-FileCopyrightText: 2024 Mass Labs
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
+// // SPDX-FileCopyrightText: 2024 Mass Labs
+// //
+// // SPDX-License-Identifier: GPL-3.0-or-later
 
-interface ValidationWarningProps {
-  warning: string | null;
-  onClose: () => void;
-}
+// interface ValidationWarningProps {
+//   warning: string | null;
+//   onClose: () => void;
+// }
 
-export default function ValidationWarning({
-  warning,
-  onClose,
-}: ValidationWarningProps) {
-  if (!warning) return null;
+// export default function ValidationWarning({
+//   warning,
+//   onClose,
+// }: ValidationWarningProps) {
+//   if (!warning) return null;
 
-  return (
-    <div className="px-4 py-2 bg-warning-yellow text-white font-thin rounded-lg flex items-center">
-      <p>{warning}</p>
-      <button onClick={() => onClose()} className="ml-auto">
-        <div className="bg-white rounded-full w-4 h-4 flex justify-center items-center">
-          <img
-            src="/icons/close-icon.svg"
-            alt="close-icon"
-            width={8}
-            height={8}
-            className="w-2 h-2"
-          />
-        </div>
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div className="px-4 py-2 bg-warning-yellow text-white font-thin rounded-lg flex items-center">
+//       <p>{warning}</p>
+//       <button onClick={() => onClose()} className="ml-auto">
+//         <div className="bg-white rounded-full w-4 h-4 flex justify-center items-center">
+//           <img
+//             src="/icons/close-icon.svg"
+//             alt="close-icon"
+//             width={8}
+//             height={8}
+//             className="w-2 h-2"
+//           />
+//         </div>
+//       </button>
+//     </div>
+//   );
+// }
--- packages/not-next/src/components/merchants/Confirmation.tsx
+++ packages/not-next/src/components/merchants/Confirmation.tsx
@@ -1,63 +1,63 @@
-// SPDX-FileCopyrightText: 2024 Mass Labs
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
+// // SPDX-FileCopyrightText: 2024 Mass Labs
+// //
+// // SPDX-License-Identifier: GPL-3.0-or-later
 
-import Link from "npm:next/link";
+// import Link from "npm:next/link";
 
-import { useShopId } from "../../hooks/useShopId.ts";
-import Button from "../common/Button.tsx";
+// import { useShopId } from "../../hooks/useShopId.ts";
+// import Button from "../common/Button.tsx";
 
-export default function Confirmation() {
-  const { shopId } = useShopId();
+// export default function Confirmation() {
+//   const { shopId } = useShopId();
 
-  function copyToClipboard() {
-    navigator.clipboard.writeText(String(shopId));
-  }
+//   function copyToClipboard() {
+//     navigator.clipboard.writeText(String(shopId));
+//   }
 
-  return (
-    <main className="pt-under-nav h-screen p-4 mt-5">
-      <section className="mt-2 flex flex-col gap-4 bg-white p-6 rounded-lg items-center">
-        <img
-          src="/icons/smiley.svg"
-          width={80}
-          height={80}
-          alt="smiley-icon"
-          className="w-auto h-auto"
-        />
-        <h1 className="font-bold" data-testid="confirmation">
-          Congratulations!
-        </h1>
-        <h1>Your shop has been created</h1>
-      </section>
-      <section className="mt-2 flex flex-col gap-4 bg-white p-6 rounded-lg ">
-        <h2 className="font-bold">shop ID</h2>
-        <p className="text-lg">
-          This is your shop’s ID. If you would like to access your shop in the
-          future, you should write this down. If your cache gets cleared, there
-          will be no other way to re-access this shop again (until we build an
-          indexer!) so we recommend that you really write it down.
-        </p>
-        <div className="flex gap-2">
-          <div className="bg-background-gray p-2 rounded-md overflow-x-auto w-40">
-            <p>{String(shopId)}</p>
-          </div>
-          <button onClick={copyToClipboard}>
-            <img
-              src="/icons/copy-icon.svg"
-              width={20}
-              height={20}
-              alt="copy-icon"
-              className="w-auto h-auto ml-auto"
-            />
-          </button>
-        </div>
-      </section>
-      <section className="mt-2 flex flex-col gap-4 bg-white p-6 rounded-lg ">
-        <h2 className="font-bold">Next steps</h2>
-        <Button>
-          <Link href="/merchant-dashboard">View Dashboard</Link>
-        </Button>
-      </section>
-    </main>
-  );
-}
+//   return (
+//     <main className="pt-under-nav h-screen p-4 mt-5">
+//       <section className="mt-2 flex flex-col gap-4 bg-white p-6 rounded-lg items-center">
+//         <img
+//           src="/icons/smiley.svg"
+//           width={80}
+//           height={80}
+//           alt="smiley-icon"
+//           className="w-auto h-auto"
+//         />
+//         <h1 className="font-bold" data-testid="confirmation">
+//           Congratulations!
+//         </h1>
+//         <h1>Your shop has been created</h1>
+//       </section>
+//       <section className="mt-2 flex flex-col gap-4 bg-white p-6 rounded-lg ">
+//         <h2 className="font-bold">shop ID</h2>
+//         <p className="text-lg">
+//           This is your shop’s ID. If you would like to access your shop in the
+//           future, you should write this down. If your cache gets cleared, there
+//           will be no other way to re-access this shop again (until we build an
+//           indexer!) so we recommend that you really write it down.
+//         </p>
+//         <div className="flex gap-2">
+//           <div className="bg-background-gray p-2 rounded-md overflow-x-auto w-40">
+//             <p>{String(shopId)}</p>
+//           </div>
+//           <button onClick={copyToClipboard}>
+//             <img
+//               src="/icons/copy-icon.svg"
+//               width={20}
+//               height={20}
+//               alt="copy-icon"
+//               className="w-auto h-auto ml-auto"
+//             />
+//           </button>
+//         </div>
+//       </section>
+//       <section className="mt-2 flex flex-col gap-4 bg-white p-6 rounded-lg ">
+//         <h2 className="font-bold">Next steps</h2>
+//         <Button>
+//           <Link href="/merchant-dashboard">View Dashboard</Link>
+//         </Button>
+//       </section>
+//     </main>
+//   );
+// }
