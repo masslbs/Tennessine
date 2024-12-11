43b2ec6e142b8bdde1bced4cdce04187363e44ee
Jinyoung <jinyoung.choi@mass.market>
2024-12-11 14:11:26 +0900

    wip

--- packages/not-next/src/App.tsx
+++ packages/not-next/src/App.tsx
@@ -1,8 +1,11 @@
 import { type Config, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+  ConnectButton,
+  getDefaultConfig,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
 import { MassMarketProvider } from "./MassMarketContext.tsx";
 import { getConfig } from "./wagmi.ts";
 import { routeTree } from "./routeTree.gen.ts";
@@ -13,7 +16,7 @@ const router = createRouter({ routeTree });
 export default function App({
   wagmiConfig = getConfig(),
 }: {
-  wagmiConfig?: Config;
+  wagmiConfig?: ReturnType<typeof getDefaultConfig> | Config;
 }) {
   return (
     <>
