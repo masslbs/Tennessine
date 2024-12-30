a66b802317faeec6ffc45d3c2ad1b4774ff0de2a
Jinyoung <jinyoung.choi@mass.market>
2024-12-30 03:54:27 -0800

    using tanstack navigate

--- packages/not-next/src/hooks/usePublicClient.ts
+++ packages/not-next/src/hooks/usePublicClient.ts
@@ -1,9 +1,8 @@
 import { usePublicClient as usePC } from "wagmi";
 import * as chains from "wagmi/chains";
-import process from "node:process";
 
 export function usePublicClient(
-  id = chains[process.env.NEXT_PUBLIC_CHAIN_NAME as keyof typeof chains].id,
+  id = chains[import.meta.env.VITE_CHAIN_NAME as keyof typeof chains]?.id,
 ) {
   const shopPublicClient = usePC({
     chainId: id,
