9ca63b53aa7f8ce6007544149c0399382f6aeffa
Jinyoung <jinyoung.choi@mass.market>
2025-01-07 10:58:56 -0800

    adding nodePolyfills to vite.config

--- packages/not-next/vite.config.ts
+++ packages/not-next/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "tailwindcss";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// https://vite.dev/config/
 export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   },
-  plugins: [deno(), react(), TanStackRouterVite({ addExtensions: true })],
+  plugins: [
+    deno(),
+    react(),
+    TanStackRouterVite({ addExtensions: true }),
+    nodePolyfills() as PluginOption,
+  ],
 });
