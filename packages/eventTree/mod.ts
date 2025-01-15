2bb9b0f20177a71eab5fd3e8622f6a5a96f4c63d
Martin Becze <martin@becze.org>
2025-01-15 12:33:34 +0000

    updated eventTree to support subscription generation

--- packages/eventTree/mod.ts
+++ packages/eventTree/mod.ts
@@ -1,74 +1,217 @@
 /**
  * This module contains Class to create a tree of events. Since we don't have a dom tree we creaete a synthetic tree that allows subscribing to events
  * along a path in the tree. When an Event is triggered on a path, it will "bubble" up the tree until it reaches the root. All events bubble up the tree.
+ * We cannot use native event emmitters because we cannot overwrite the `event.target` property and we need to know the listener count to manage subscriptions.
+ *
+ * ## Subscriptions
+ * We can utilize the event tree to manage subscriptions to the relay(s). When listners are added and removed an event is emmited on the root node's `meta` event emmiter.
+ * This event contains an array of `SubscriptionUpdate` objects that describe the changes to the subscriptions.
+ * `SubscriptionUpdate`s are aware of the path in the tree where the subscription was added or removed and will not emit events for children nodes if the parent node has a subscription.
  * @module
  */
 import jsonpointer from "npm:@sagold/json-pointer";
 
-const eventTargets = new WeakMap<Event, Tree>();
+/** A callback function that gets called when an event is emitted */
+export type EventListener<T> = (
+  event: T,
+  source: EventEmmiter<T> | object,
+) => void;
 
-/** This class dispatch's an {@link Event} and bubbles that event up the tree */
-export class Tree<T extends Event = Event> extends EventTarget {
-  #map = new Map<string, Tree>();
+/** Used to express changes to which part of the tree is needs a subscription to */
+export interface SubscriptionUpdate {
+  /** Whether to add a subscription or remove a subscription */
+  subscribe: boolean;
+  /** the path on the object tree to subscribe/unsubscribe */
+  path: string;
+}
+
+/**
+ * A very simple class that emits events
+ * @template T the type of the event
+ */
+export class EventEmmiter<T> {
+  /** A map of listners */
+  listners = new Set<EventListener<T>>();
+
+  /** registers a callback to listen to events */
+  on(listener: EventListener<T>) {
+    this.listners.add(listener);
+  }
+
+  /** removes a callback  */
+  off(listener: EventListener<T>) {
+    this.listners.delete(listener);
+  }
+
+  /** emits an event  */
+  emit(event: T, source: object = this) {
+    this.listners.forEach((listener) => {
+      listener(event, source);
+    });
+  }
+}
+
+/**
+ * This class dispatch's an event and bubbles that event up the tree
+ * @template T the type of the event
+ */
+export class Tree<T> {
+  #edges = {} as Record<string, Tree<T>>;
+  /** The event emmiter for this node */
+  readonly emmiter = new EventEmmiter<T>();
+  /** The event emmiter for the meta events, this only works on the root node of the tree */
+  readonly meta = new EventEmmiter<SubscriptionUpdate[]>();
   /** create a new instance of the Tree */
   constructor(
+    /** The name of the node, "" for root */
     readonly name: string = "",
-    readonly parentNode: Tree | null = null,
-  ) {
-    super();
-  }
+    /** The parent node, null for root */
+    readonly parentNode: Tree<T> | null = null,
+  ) {}
 
   /** returns the node's path in a tree */
   get path(): string {
-    return this.#path(true);
+    return "/" + this.#path().join("/");
   }
 
-  #path(first: boolean): string {
-    if (this.parentNode) {
-      return this.parentNode.#path(false).concat("/", this.name);
-    } else if (first) {
-      return "/";
+  /** returns the root node of the tree */
+  get root(): Tree<T> {
+    return this.parentNode?.root ?? this;
+  }
+
+  #path(): string[] {
+    return this.parentNode ? [...this.parentNode.#path(), this.name] : [];
+  }
+
+  /** returns true if the node or the nodes parent has listeners */
+  hasListeners(): boolean {
+    return this.emmiter.listners.size > 0 ||
+      (this.parentNode?.hasListeners() ?? false);
+  }
+
+  #getChildren(): Tree<T>[] {
+    return Object.values(this.#edges).reduce(
+      (acc, edge) =>
+        acc.concat(edge.emmiter.listners.size ? [edge] : edge.#getChildren()),
+      [] as Tree<T>[],
+    );
+  }
+
+  /** subscribes to an event on a path in the tree */
+  on(
+    ...args: [path: string | string[], listener: EventListener<T>] | [
+      listener: EventListener<T>,
+    ]
+  ): Tree<T> {
+    const [path, listener] = args.length === 1 ? ["", args[0]] : args;
+    const p: string[] = jsonpointer.split(path);
+    if (p.length) {
+      return this.#getOrExtendPath(p).on(p, listener);
     } else {
-      return "";
+      if (!this.hasListeners()) {
+        const update: SubscriptionUpdate[] = [];
+        update.push({
+          subscribe: true,
+          path: this.path,
+        });
+
+        //  remove subscription from children nodes
+        this.#getChildren().forEach((child) => {
+          update.push({
+            subscribe: false,
+            path: child.path,
+          });
+        });
+        this.root.meta.emit(update);
+      }
+      this.emmiter.on(listener);
+      return this;
+    }
+  }
+
+  #removeEdge(edge: string) {
+    delete this.#edges[edge];
+    if (!Object.keys(this.#edges).length && this.parentNode) {
+      this.parentNode.#removeEdge(this.name);
+    }
+  }
+
+  /** unsubscribes from an event on a path in the tree */
+  off(
+    ...args: [path: string | string[], listener: EventListener<T>] | [
+      listener: EventListener<T>,
+    ]
+  ): Tree<T> | void {
+    const [path, listener] = args.length === 1 ? ["", args[0]] : args;
+    const p: string[] = jsonpointer.split(path);
+    if (p.length) {
+      return this.#getPath(p)?.off(p, listener);
+    } else {
+      this.emmiter.off(listener);
+      if (!Object.keys(this.#edges).length && this.parentNode) {
+        this.parentNode.#removeEdge(this.name);
+      }
+      if (!this.hasListeners()) {
+        const update: SubscriptionUpdate[] = [];
+        update.push({
+          subscribe: false,
+          path: this.path,
+        });
+        this.#getChildren().forEach((child) => {
+          update.push({
+            subscribe: true,
+            path: child.path,
+          });
+        });
+        this.root.meta.emit(update);
+      }
+      return this;
+    }
+  }
+
+  /** emits an event on a path in the tree */
+  emit(
+    ...args: [path: string | string[], event: T] | [event: T]
+  ): Tree<T> | void {
+    const [path, event] = args.length === 1 ? [[], args[0]] : args;
+    const p: string[] = jsonpointer.split(path);
+    if (p.length) {
+      return this.#getOrExtendPath(p).emit(event);
+    } else {
+      this.emmiter.emit(event, this);
+      this.parentNode?.emit(event);
+      return this;
     }
   }
 
   /** get a node in the tree by path */
-  get(path: string | string[]): Tree {
+  get(path: string | string[]): Tree<T> | undefined {
     const p: string[] = jsonpointer.split(path);
     return this.#getPath(p);
   }
 
-  #getPath(p: string[]): Tree {
+  #getPath(p: string[]): Tree<T> | undefined {
     if (p.length) {
-      const edge = p.shift()!;
-      if (!this.#map.has(edge)) {
-        this.#map.set(edge, new Tree(edge, this));
+      const edge = p.shift();
+      const next = this.#edges[edge!];
+      if (next) {
+        return next.#getPath(p);
       }
-      const next = this.#map.get(edge)!;
-      return next.#getPath(p);
     } else {
       return this;
     }
   }
 
-  /**
-   * dispatches an event on the tree and bubbles it up the tree
-   * @inheritdoc
-   */
-  override dispatchEvent(event: T): boolean {
-    if (!eventTargets.has(event)) {
-      eventTargets.set(event, this);
+  #getOrExtendPath(p: string[]): Tree<T> {
+    if (p.length) {
+      const edge = p.shift()!;
+      if (!this.#edges[edge]) {
+        this.#edges[edge] = new Tree(edge, this);
+      }
+      const next = this.#edges[edge];
+      return next.#getOrExtendPath(p)!;
+    } else {
+      return this;
     }
-    return super.dispatchEvent(event) && (
-      this.parentNode?.dispatchEvent(event) ?? true
-    );
-  }
-  /**
-   * since we cannot overwrite `event.target` we have to use an alternative way to get and set event targets.
-   * This static method allows to get the target of an event set by the tree.
-   */
-  static getTarget(event: Event): Tree | undefined {
-    return eventTargets.get(event);
   }
 }
--- packages/eventTree/mod_test.ts
+++ packages/eventTree/mod_test.ts
@@ -1,29 +1,82 @@
-import { Tree } from "./mod.ts";
+import { EventEmmiter, Tree } from "./mod.ts";
 import { assertEquals } from "jsr:@std/assert";
 
 Deno.test("Tree Event Testings", async (t) => {
-  await t.step("", async () => {
+  await t.step("basic eventemmiter, no tree", () => {
+    const emmiter = new EventEmmiter<string>();
+    let val;
+    const func = (e: string) => {
+      val = e;
+    };
+    emmiter.on(func);
+    emmiter.emit("test");
+    assertEquals(val, "test", "Event should be dispatched");
+    emmiter.off(func);
+    emmiter.emit("test2");
+    assertEquals(val, "test", "Handler should be removed");
+  });
+  await t.step("Tree Events!", async () => {
     const tree = new Tree();
     assertEquals(tree.path, "/");
-    const f = tree.get("/a/b/c/d/e/f");
-    const p = new Promise((resolve) => {
-      f.addEventListener("test", resolve);
-    });
-    let event = new Event("test");
-    f.dispatchEvent(event);
-    const val = await p;
+    const { promise, resolve } = Promise.withResolvers();
+    let event = "test";
+    const f = tree.on("a/b/c/d/e/f", resolve);
+    f.emit(event);
+    const val = await promise;
     assertEquals(val, event, "Event should be dispatched");
 
-    const d = tree.get("/a/b/c/d");
+    const d = tree.get("/a/b/c/d")!;
     assertEquals(d.path, "/a/b/c/d");
-    const pd = new Promise<Event>((resolve) => {
-      d.addEventListener("test", resolve);
+    let val2;
+    d.on((e) => {
+      val2 = e;
     });
-    event = new Event("test");
-    f.dispatchEvent(event);
-    const val2 = await pd;
+    event = "test2";
+    f.emit(event);
+    // const val2 = await promise;
     assertEquals(val2, event, "Event should be dispatched to parents");
-    console.log(Tree.getTarget(val2)!.path);
-    assertEquals(Tree.getTarget(val2), f);
+  });
+  await t.step("meta: subscription observations", () => {
+    const tree = new Tree<string>();
+    let subArray;
+    tree.meta.on((e) => {
+      subArray = e;
+    });
+    let val;
+    const func = (e: string) => {
+      val = e;
+    };
+    const f = tree.on("a/b/c/d/e/f", func);
+    assertEquals(subArray, [{ subscribe: true, path: "/a/b/c/d/e/f" }]);
+
+    tree.on("a/b", func);
+    assertEquals(subArray, [
+      { subscribe: true, path: "/a/b" },
+      {
+        subscribe: false,
+        path: "/a/b/c/d/e/f",
+      },
+    ]);
+
+    tree.off("a/b", func);
+
+    assertEquals(subArray, [
+      { subscribe: false, path: "/a/b" },
+      {
+        subscribe: true,
+        path: "/a/b/c/d/e/f",
+      },
+    ]);
+
+    f.off(func);
+    assertEquals(subArray, [
+      {
+        subscribe: false,
+        path: "/a/b/c/d/e/f",
+      },
+    ]);
+
+    tree.emit("a/b/c/d/e/f", "test");
+    assertEquals(val, undefined, "Event should not be dispatched");
   });
 });
