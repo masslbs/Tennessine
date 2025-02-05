a666a10556869123d0ad66b38d902ba1ba90e121
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-02-05 11:04:41 -0800

    wip

--- packages/not-next/src/components/merchants/MerchantConnect_test.tsx
+++ packages/not-next/src/components/merchants/MerchantConnect_test.tsx
@@ -1,26 +1,21 @@
 import "../../happyDomSetup.ts";
+
 import { cleanup, render, screen } from "npm:@testing-library/react";
-import { hardhat } from "wagmi/chains";
-import { mock } from "@wagmi/connectors";
-import { connect, createConfig, createTransport, http } from "@wagmi/core";
 import MerchantConnect from "./MerchantConnect.tsx";
 import { createRouterWrapper } from "../../utils/test.tsx";
-import { custom } from "wagmi";
 
-// Add this before your test
-
-Deno.test("Check that we can render the merchant connect page", async (t) => {
+Deno.test("Check that we can render the merchant connect page", {
+  sanitizeResources: false,
+  sanitizeOps: false
+}, async (t) => {
   const wrapper = createRouterWrapper(null, "/merchant-connect");
 
   await t.step("Render and unmount component", async () => {
     const { unmount } = render(<MerchantConnect />, { wrapper });
     screen.getByTestId("merchant-connect-page");
 
-    // Proper cleanup sequence
     unmount();
     cleanup();
-
-    // Allow any pending timers/microtasks to resolve
-    await new Promise((resolve) => setTimeout(resolve, 0));
+    await new Promise((resolve) => setTimeout(resolve, 200));
   });
 });
