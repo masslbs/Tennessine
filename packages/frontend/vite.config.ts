8af00eafbddc31b34566cc417f8d1d7d378010ec
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-02-12 13:03:26 -0800

    adding entrypoint in vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,16 +5,36 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "tailwindcss";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
+const entryPoints = ['listings']
 export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        listings: 'index.html',
+      }
+    },
+    outDir: 'dist',
+    copyPublicDir: true,
+  },
+
   plugins: [
     deno(),
     react(),
     TanStackRouterVite({ addExtensions: true }),
     nodePolyfills() as PluginOption,
+    {
+      name: 'copy-html-to-subdirs',
+      async closeBundle() {
+        for (const entry of entryPoints) {
+          await Deno.mkdir(`dist/${entry}`, { recursive: true });
+          await Deno.copyFile('dist/index.html', `dist/${entry}/index.html`);
+        }
+      }
+    }
   ],
 });
