b0a51bcef682f8bfef28ff787fb85f2fc0dff9a5
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-02-14 06:31:43 -0800

    create script to run after build cmd

---
+++ packages/frontend/scripts/generate-entry-points.ts
@@ -0,0 +1,11 @@
+import { routeTree } from "../src/routeTree.gen.ts";
+
+// Slice off the leading slash for each path and remove the index route. 
+const entryPoints = Object.values(routeTree.children || {})
+  .map((r) => r.options.path.slice(1))
+  .slice(1);
+
+for (const entry of entryPoints) {
+  await Deno.mkdir(`dist/${entry}`, { recursive: true });
+  await Deno.copyFile('dist/index.html', `dist/${entry}/index.html`);
+} 
