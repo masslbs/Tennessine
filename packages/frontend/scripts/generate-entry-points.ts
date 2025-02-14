9c7174e077361baf26a8e54fbbb5d49b680b5271
Henry <cryptix@riseup.net>
2025-02-14 17:43:19 +0100

    bundle entryPoints extraction

--- packages/frontend/scripts/generate-entry-points.ts
+++ packages/frontend/scripts/generate-entry-points.ts
@@ -1,11 +1,15 @@
 import { routeTree } from "../src/routeTree.gen.ts";
 
 // Slice off the leading slash for each path and remove the index route.
-const entryPoints = Object.values(routeTree.children || {})
+export const entryPoints = Object.values(routeTree.children || {})
   .map((r) => r.options.path.slice(1))
   .slice(1);
 
-for (const entry of entryPoints) {
-  await Deno.mkdir(`dist/${entry}`, { recursive: true });
-  await Deno.copyFile("dist/index.html", `dist/${entry}/index.html`);
+// usually run by deno task generate, as part of the production pipline
+if (import.meta.main) {
+  console.log("Copying index.html for each entry point...");
+  for (const entry of entryPoints) {
+    await Deno.mkdir(`dist/${entry}`, { recursive: true });
+    await Deno.copyFile("dist/index.html", `dist/${entry}/index.html`);
+  }
 }
