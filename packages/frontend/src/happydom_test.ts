47946737d4584f7c29f25972178530512dd05ec2
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-02-20 09:05:25 -0800

    happydom uses native deno fetch api

---
+++ packages/frontend/src/happydom_test.ts
@@ -0,0 +1,38 @@
+import "./happyDomSetup.ts";
+
+import { expect } from "jsr:@std/expect";
+import { createPublicClient, createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { hardhat } from "viem/chains";
+
+import { anvilPrivateKey } from "@massmarket/contracts";
+import { random256BigInt } from "@massmarket/utils";
+import { mintShop } from "@massmarket/blockchain";
+
+const account = privateKeyToAccount(
+  anvilPrivateKey,
+);
+
+const shopId = random256BigInt();
+Deno.test("Fetch API works with happydom", {
+  sanitizeResources: false,
+  sanitizeOps: false,
+}, async () => {
+  const wallet = createWalletClient({
+    account,
+    chain: hardhat,
+    transport: http(),
+  });
+  const publicClient = createPublicClient({
+    chain: hardhat,
+    transport: http(),
+  });
+  const transactionHash = await mintShop(wallet, [
+    shopId,
+    wallet.account.address,
+  ]);
+  const receipt = await publicClient.waitForTransactionReceipt({
+    hash: transactionHash,
+  });
+  expect(receipt.status).toBe("success");
+});
