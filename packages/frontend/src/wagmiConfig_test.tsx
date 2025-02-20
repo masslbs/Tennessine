b5f48bef2a10fa88ccb2a2c819ff91afbbcfbb45
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-02-20 13:39:11 -0800

    test wallet client wagmi config

---
+++ packages/frontend/src/wagmiConfig_test.tsx
@@ -0,0 +1,35 @@
+import "./happyDomSetup.ts";
+import { render, screen, waitFor } from "@testing-library/react";
+import { useAccount, useWalletClient } from "wagmi";
+import { config, createRouterWrapper } from "./utils/test.tsx";
+import { expect } from "jsr:@std/expect";
+import { connect } from "npm:wagmi/actions";
+import { hardhat } from "wagmi/chains";
+
+const TestComponent = () => {
+  const { status } = useAccount();
+  const { data: wallet } = useWalletClient();
+
+  return (
+    <div>
+      <p>{status}</p>
+      <p>{wallet?.chain?.id}</p>
+    </div>
+  );
+};
+
+Deno.test("Test wallet client is configured correctly", {
+  sanitizeResources: false,
+  sanitizeOps: false,
+}, async () => {
+  const { wrapper } = await createRouterWrapper();
+  render(<TestComponent />, { wrapper });
+  await connect(config, { connector: config.connectors[0] });
+
+  await waitFor(() => {
+    expect(screen.getByText("connected")).toBeTruthy();
+  });
+  await waitFor(() => {
+    expect(screen.getByText(hardhat.id)).toBeTruthy();
+  });
+});
