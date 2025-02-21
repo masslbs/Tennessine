1c66cfbcdb7b799368218d81e179bda0ef7e4dd6
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-02-21 08:37:15 -0800

    replace fetch and other web apis

--- packages/frontend/src/happyDomSetup.ts
+++ packages/frontend/src/happyDomSetup.ts
@@ -1,3 +1,14 @@
-// register dom globals
 import { GlobalRegistrator } from "npm:@happy-dom/global-registrator";
+
+// Store deno's web APIs and reassign them after happydom is registered
+const denoFetch = globalThis.fetch;
+const denoHeaders = globalThis.Headers;
+const denoRequest = globalThis.Request;
+const denoAbortController = globalThis.AbortController;
+
 GlobalRegistrator.register();
+
+globalThis.AbortController = denoAbortController;
+globalThis.Headers = denoHeaders;
+globalThis.Request = denoRequest;
+globalThis.fetch = denoFetch;
