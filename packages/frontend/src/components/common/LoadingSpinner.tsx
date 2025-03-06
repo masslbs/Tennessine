eee57bd3be883e8f67976cc0ae2914a0c7086e4e
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-03-06 09:14:46 -0800

    loading spinner

--- packages/frontend/src/components/common/LoadingSpinner.tsx
+++ packages/frontend/src/components/common/LoadingSpinner.tsx
@@ -1,9 +1,24 @@
-import { Spinner } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 export default function LoadingSpinner() {
+  const [currentSpinner, setCurrentSpinner] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSpinner((prev) => (prev % 12) + 1);
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+    <div className="w-10 h-10">
+      <img
+        src={`/icons/spinners/spinner-${currentSpinner}.svg`}
+        alt="Loading spinner"
+        width={80}
+        height={80}
+      />
+    </div>
   );
 }
