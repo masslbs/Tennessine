824eaa96397049b98bf73ff61ba11c9ea92c5f6b
Martin Becze <martin@becze.org>
2025-03-10 18:18:44 +0000

    remove token addresses

--- packages/contracts/mod.ts
+++ packages/contracts/mod.ts
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Mass Labs
 //
 // SPDX-License-Identifier: Unlicense
-
-import contractAddresses from "./deploymentAddresses.json" with {
-  type: "json",
-};
 import tokenAddresses from "./tokenAddresses.json" with { type: "json" };
 export { tokenAddresses };
 export * from "./src/generated.ts";
@@ -20,10 +16,3 @@ export const permissions = {
   removeUser: 7,
   publishInviteVerifier: 8,
 } as const;
-
-const _addresses = {
-  ...contractAddresses,
-};
-
-type Addresses<Object> = Record<keyof Object, `0x${string}`>;
-export const addresses = _addresses as Addresses<typeof _addresses>;
