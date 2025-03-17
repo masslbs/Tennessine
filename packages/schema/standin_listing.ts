2aececeb5092456b29190163135fe4dccc1ff567
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-03-17 08:38:30 -0700

    create baseClass for returnAsMap fn

--- packages/schema/standin_listing.ts
+++ packages/schema/standin_listing.ts
@@ -1,15 +1,21 @@
 import { PriceModifier } from "./standin_manifest.ts";
+import { BaseClass } from "./utils.ts";
+export interface ListingStockStatus {
+  variationIds: string[];
+  inStock: boolean | null;
+  expectedInStockBy: Date | null;
+}
 
-export class Listing {
+export class Listing extends BaseClass {
   ID: number;
   Price: bigint;
   Metadata: ListingMetadata;
   ViewState: ListingViewState;
   Options: Map<string, ListingOption>;
-  StockStatuses: Map<string, ListingStockStatus>;
+  StockStatuses: Map<string, ListingStockStatus[]>;
 
-  constructor(public input: Map<string, any>) {
-    // console.log("listing input", input);
+  constructor(input: Map<string, any>) {
+    super();
     this.ID = input.get("ID");
     this.Price = input.get("Price");
     this.Metadata = new ListingMetadata(input.get("Metadata"));
@@ -19,21 +25,16 @@ export class Listing {
   }
 }
 
-export class ListingMetadata {
+export class ListingMetadata extends BaseClass {
   Title: string;
   Description: string;
-  Images: URL[];
+  Images: string[];
 
-  constructor(public input: Map<string, any>) {
-    // console.log("metadata input", input);
+  constructor(input: Map<string, any>) {
+    super();
     this.Title = input.get("Title");
     this.Description = input.get("Description");
-
-    // Ensure all images are valid URLs
-    const images = input.get("Images") ?? [];
-    this.Images = images.map((img: string) => {
-      return new URL(img);
-    });
+    this.Images = input.get("Images") ?? [];
   }
 }
 
@@ -56,22 +57,24 @@ export function ViewStateFromNumber(num: number): ListingViewState {
   }
 }
 
-export class ListingOption {
+export class ListingOption extends BaseClass {
   Title: string;
   Variations: Map<string, ListingVariation>;
 
-  constructor(public input: Map<string, any>) {
+  constructor(input: Map<string, any>) {
+    super();
     this.Title = input.get("Title");
     this.Variations = input.get("Variations");
   }
 }
 
-export class ListingVariation {
+export class ListingVariation extends BaseClass {
   VariationInfo: ListingMetadata;
   PriceModifier: PriceModifier;
   SKU: string;
 
-  constructor(public input: Map<string, any>) {
+  constructor(input: Map<string, any>) {
+    super();
     this.VariationInfo = new ListingMetadata(input.get("VariationInfo"));
     this.PriceModifier = new PriceModifier(input.get("PriceModifier"));
     this.SKU = input.get("SKU");
--- packages/stateManager/mod_test.ts
+++ packages/stateManager/mod_test.ts
@@ -1,108 +1,74 @@
-import { hexToBytes } from "@wevm/viem";
 import { assertEquals } from "@std/assert";
+
 import { MemStore } from "@massmarket/merkle-dag-builder/memstore";
-import type { codec } from "@massmarket/utils";
 import { createTestClients } from "@massmarket/client/test";
+import { extractEntriesFromHAMT, fetchAndDecode } from "@massmarket/utils";
+
 import StateManager from "./mod.ts";
+import { Listing } from "../schema/standin_listing.ts";
+import { Manifest } from "../schema/standin_manifest.ts";
+import { Order } from "../schema/standin_order.ts";
 
-// const ManifestOkayTest = await fetch(
-//   `file://${Deno.env.get("MASS_TEST_VECTORS")}/vectors/ManifestOkay.cbor`,
-// );
-
-// const ManifestOkayTestBytes = await ManifestOkayTest.bytes();
-// const manifest = decodeCbor(ManifestOkayTestBytes);
-
-const manifest = new Map<string, codec.CodecValue>([
-  ["ShopID", 2463539455555n],
-  [
-    "Payees",
-    new Map([
-      [
-        1337,
-        new Map([
-          [
-            hexToBytes("0x0000000000000000000000000000000000000000"),
-            new Map<string, codec.CodecValue>([
-              ["isContract", true],
-              ["description", "My main wallet"],
-            ]),
-          ],
-        ]),
-      ],
-    ]),
-  ],
-  [
-    "AcceptedCurrencies",
-    new Map([
-      [
-        1337,
-        new Map([
-          [hexToBytes("0x0000000000000000000000000000000000000000"), null],
-          [hexToBytes("0x0000000000000000000000000000000000000001"), null],
-          [hexToBytes("0x0000000000000000000000000000000000000002"), null],
-        ]),
-      ],
-      [
-        2,
-        new Map([
-          [hexToBytes("0x0000000000000000000000000000000000000000"), null],
-        ]),
-      ],
-    ]),
-  ],
-  [
-    "PricingCurrency",
-    new Map<string, codec.CodecValue>([
-      ["ChainID", 1337],
-      ["Address", "0x0000000000000000000000000000000000000000"],
-    ]),
-  ],
-  [
-    "ShippingRegions",
-    new Map([
-      [
-        "default",
-        new Map([
-          ["Country", "DE"],
-          ["Postcode", ""],
-          ["City", ""],
-          ["PriceModifiers", null],
-        ]),
-      ],
-    ]),
-  ],
-]);
-
-// const listing1 = {
-//   "ID": 234,
-//   "Name": "testingvalue",
-//   "Metadata": {
-//     "Title": "testingvalue",
-//     "Description": "testingvalue",
-//     "Images": ["testingvalue"],
-//   },
-//   "Price": 100,
-//   "ViewState": 1,
-//   "Options": [],
-//   "StockStatus": "In Stock",
-// };
-
-const store = new MemStore();
 Deno.test("Database Testings", async (t) => {
-  await t.step("create a database and a Manifest", async () => {
-    const sm = new StateManager({
-      store,
-      objectId: manifest.get("ShopID") as bigint,
-    });
-    const { resolve, promise } = Promise.withResolvers();
-    sm.events.on(["Manifest"], resolve);
-    await sm.set(["Manifest"], manifest);
-    const result = await sm.get(["Manifest"]);
-    assertEquals(result, manifest);
-    const pr = await promise;
-    assertEquals(result, pr);
+  const manifestVector = await fetchAndDecode("ManifestOkay");
+  const manifests = manifestVector.get("Snapshots")?.map((snapshot: any) => {
+    return snapshot.get("After").get("Value").get("Manifest");
+  }) || [];
+  const store = new MemStore();
+  const db = new StateManager({
+    store,
+    objectId: manifests[0].get("ShopID") as bigint,
+  });
+  // Need to initialize the listings map
+  await db.set(["Listings"], new Map());
+  await db.set(["Orders"], new Map());
+
+  await t.step("Set Manifest", async () => {
+    const unpacked = new Manifest(manifests[0]);
+    const mapped = unpacked.returnAsMap();
+    await db.set(["Manifest"], mapped);
+
+    const result = await db.get(["Manifest"]);
+    assertEquals(result, manifests[0]);
   });
 
+  await t.step("Set Listings", async () => {
+    const ListingsVector = await fetchAndDecode("ListingOkay");
+    const listings = ListingsVector?.get("Snapshots")?.map((snapshot: any) => {
+      const hamtNode = snapshot?.get("After")?.get("Value")?.get("Listings");
+      return extractEntriesFromHAMT(hamtNode);
+    }) || [];
+    for (const listingMap of listings) {
+      for (const [id, listing] of listingMap.entries()) {
+        const unpacked = new Listing(listing);
+        const mapped = unpacked.returnAsMap();
+        await db.set(["Listings", id], mapped);
+        const result = await db.get(["Listings", id]);
+        assertEquals(result, mapped);
+      }
+    }
+    await db.get(["Listings"]);
+  });
+
+  await t.step("Set Orders", async () => {
+    const OrderVector = await fetchAndDecode("OrderOkay");
+    const orders = OrderVector?.get("Snapshots")?.map((snapshot: any) => {
+      const hamtNode = snapshot?.get("After")?.get("Value")?.get("Orders");
+      return extractEntriesFromHAMT(hamtNode);
+    }) || [];
+
+    for (const orderMap of orders) {
+      for (const [id, order] of orderMap.entries()) {
+        const unpacked = new Order(order);
+        const mapped = unpacked.returnAsMap();
+        await db.set(["Orders", id], mapped);
+
+        const result = await db.get(["Orders", id]);
+        assertEquals(result, mapped);
+      }
+    }
+    await db.get(["Orders"]);
+  });
   await t.step("add a relay and set a key and retrieve it", async () => {
     const { relayClient } = await createTestClients();
     const sm = new StateManager({
