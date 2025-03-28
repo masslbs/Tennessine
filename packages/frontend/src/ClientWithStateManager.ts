0c15b81a6297938ce9a86c2afa63c0964c26bd88
Henry <henry@mass.market>
2025-03-28 18:19:33 +0100

    some field initialization safty

--- packages/frontend/src/ClientWithStateManager.ts
+++ packages/frontend/src/ClientWithStateManager.ts
@@ -5,8 +5,8 @@ import { type IRelayEndpoint, RelayClient } from "@massmarket/client";
 import { LevelStore } from "@massmarket/store/level";
 
 export class ClientWithStateManager {
-  public stateManager: Database;
-  public relayClient: RelayClient;
+  #stateManager?: Database;
+  #relayClient?: RelayClient;
 
   constructor(
     private readonly relayEndpoint: IRelayEndpoint,
@@ -25,15 +25,15 @@ export class ClientWithStateManager {
       Manifest: new Map(),
       SchemeVersion: 1,
     }));
-    this.stateManager = new Database({
+    this.#stateManager = new Database({
       store: new LevelStore(),
       objectId: this.shopId,
       root,
     });
 
-    await this.stateManager.open();
+    await this.#stateManager.open();
 
-    this.relayClient = new RelayClient({
+    this.#relayClient = new RelayClient({
       relayEndpoint: this.relayEndpoint,
       walletClient: this.walletClient,
       keycard: this.account,
@@ -41,11 +41,28 @@ export class ClientWithStateManager {
     });
   }
 
+  get stateManager() {
+    if (!this.#stateManager) {
+      throw new Error("state manager not open");
+    }
+    return this.#stateManager;
+  }
+
+  get relayClient() {
+    if (!this.#relayClient) {
+      throw new Error("relay client not open");
+    }
+    return this.#relayClient;
+  }
+
   addConnection() {
-    if (!this.relayClient) {
+    if (!this.#relayClient) {
       throw new Error("No relay client");
     }
+    if (!this.#stateManager) {
+      throw new Error("state manager not open");
+    }
 
-    return this.stateManager.addConnection(this.relayClient);
+    return this.#stateManager.addConnection(this.#relayClient);
   }
 }
