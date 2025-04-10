bd5dbb3c4d6bc1b90655d2385f736a3d36ba6ee1
Alp <alp@mass.market>
2025-04-10 13:56:13 +0200

    demo: vite env proof of concept
    This commit demonstrates one workaround for getting access to the
    dotenv files exposed to vite.
    
    It works by adapting vite's `defineConfig`, and passing a callback into
    it and extracting the `mode` parameter passed to the callback. `mode`'s
    value is either `development` or `production`, and we want to pick an
    `.env` file depending on the value of `mode`.
    
    One way to do this--as demonstrated in this proof of concept--is to call `loadEnv()` with our `mode` variable, and use the returned value to define a global on the `window` which will be accessible to the app in the browser.
    
    In a browser session, logging window.__MODE__ will give you the mode the
    app is executing as, and window.__ENV__ will give you the environment
    variables defined in the corresponding .env file (either
    .env.development or .env.production).

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig, loadEnv, PluginOption } from "vite";
+import { cwd } from "node:process";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -12,26 +13,34 @@ const buildInputs = Object.fromEntries(
   entryPoints.map((path) => [path, "index.html"]),
 );
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: buildInputs,
+/* proof of concept for vite's modes :) */
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd());
+  return {
+    define: {
+      __ENV__: JSON.stringify(env), // puts the env on window.__ENV__
+      __MODE__: JSON.stringify(mode), // puts the vite mode (development or production) on window.__MODE__
     },
-    outDir: "dist",
-    copyPublicDir: true,
-  },
-  plugins: [
-    deno() as PluginOption,
-    react(),
-    TanStackRouterVite({ addExtensions: true }),
-    nodePolyfills() as PluginOption,
-    tailwindcss() as PluginOption,
-  ],
-  optimizeDeps: {
-    include: [
-      // This prebundles the secp256k1 dependency which is used by some functions imported by viem (e.g. recoverPublicKey)
-      // Since the imported function uses dynamic import of this dependency. Dynamic import of modules seems to cause errors with vite.
-      "@noble/secp256k1",
+    build: {
+      rollupOptions: {
+        input: buildInputs,
+      },
+      outDir: "dist",
+      copyPublicDir: true,
+    },
+    plugins: [
+      deno() as PluginOption,
+      react(),
+      TanStackRouterVite({ addExtensions: true }),
+      nodePolyfills() as PluginOption,
+      tailwindcss() as PluginOption,
     ],
-  },
+    optimizeDeps: {
+      include: [
+        // This prebundles the secp256k1 dependency which is used by some functions imported by viem (e.g. recoverPublicKey)
+        // Since the imported function uses dynamic import of this dependency. Dynamic import of modules seems to cause errors with vite.
+        "@noble/secp256k1",
+      ],
+    },
+  };
 });
