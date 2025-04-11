cfb9f906fc70fac2be62d9ca32102e84e7846582
Henry <henry@mass.market>
2025-04-11 14:43:06 +0200

    import.meta.env type checking trickery

--- packages/frontend/src/utils/env.ts
+++ packages/frontend/src/utils/env.ts
@@ -12,9 +12,7 @@ import { envConfig as envVite } from "./env-vite.ts";
 import { envConfig as envProd } from "./env-production.ts";
 
 let mode = "development";
-// somehow doesn't work
-// const viteEnv = import.meta as unknown as { env: { MODE: string } };
-const viteEnv = import.meta.env;
+const viteEnv = (import.meta as unknown as { env: { MODE: string } }).env;
 if (viteEnv && viteEnv.MODE) {
   mode = viteEnv.MODE;
 } else if (typeof Deno !== "undefined" && Deno.env.get("MODE")) {
