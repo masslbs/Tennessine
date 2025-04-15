ecfc8a4a832584ecd65842050c744326ac18024c
Alp <alp@mass.market>
2025-04-15 12:08:29 +0200

    fix(merchant): listings view better accomodates small-medium viewports
    this change fixes products being squashed horizontally for viewports
    that are medium-small size. the squashing happened due to the grid being
    split into three parts before the before the mobile-first
    one-product-per-row breakpoint has been hit. the issue could be seen
    when viewing a shop in a browser window that was only half the screen.
    
    with this change there's a default set as one-product per row, which
    then goes into a grid of two for small viewports and continues on this
    two-grid until hitting the breakpoint for larger viewports, which is
    when the grid is divide into parts of three instead.

--- packages/frontend/src/components/merchants/listings/MerchantViewListings.tsx
+++ packages/frontend/src/components/merchants/listings/MerchantViewListings.tsx
@@ -128,7 +128,7 @@ export default function MerchantViewProducts({
         </Button>
       </div>
       <section className="mt-2 flex flex-col gap-4 bg-white p-5 rounded-lg">
-        <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {renderProducts()}
         </section>
       </section>
