132702fdf27e205a3f75cbccb8d24e8315fa22d1
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-04-18 20:18:00 +0100

    resolve conflict

--- packages/stateManager/mod_test.ts
+++ packages/stateManager/mod_test.ts
@@ -26,158 +26,138 @@ const root = new Map(Object.entries({
   SchemeVersion: 1,
 }));
 
-// TODO: there is a infrequent leak in this test, but it's not clear what is causing it.
-//
-// Database Testings => ./mod_test.ts:29:6
-// error: Leaks detected:
-//   - A fetch response body was created before the test started, but was consumed during the test. Do not close resources in a test that were not created during that test.
-const testOpts = {
-  name: "Relay Client: Unit Tests",
-  sanitizeResources: false,
-  sanitizeOps: false,
-};
+Deno.test("Database Testings", async (t) => {
+  const store = new MemStore();
+  const sm = new StateManager({
+    store,
+    id: relayClient.shopId,
+    defaultState: root,
+  });
 
-Deno.test(
-  {
-    ...testOpts,
-    async fn(t) {
-      const store = new MemStore();
-      const sm = new StateManager({
-        store,
-        id: relayClient.shopId,
-        defaultState: root,
-      });
+  await sm.open();
 
-      await sm.open();
+  await t.step("add a relay and set a key and retrieve it", async () => {
+    // connect to the relay
+    const { resolve, promise } = Promise.withResolvers();
+    sm.events.on((manifestPatch) => {
+      resolve(manifestPatch);
+    }, ["Manifest"]);
 
-      await t.step("add a relay and set a key and retrieve it", async () => {
-        // connect to the relay
-        const { resolve, promise } = Promise.withResolvers();
-        sm.events.on((manifestPatch) => {
-          resolve(manifestPatch);
-        }, ["Manifest"]);
+    sm.addConnection(relayClient);
+    // wait for manifest to be received
+    await promise;
+    const testAddr = Uint8Array.from([
+      0xf0,
+      0xf1,
+      0xf2,
+      0x03,
+      0x04,
+      0x05,
+      0xf6,
+      0xf7,
+      0xf8,
+      0x09,
+      0x0a,
+      0x0b,
+      0xfc,
+      0xfd,
+      0xfe,
+      0x0f,
+      0x01,
+      0x02,
+      0xf3,
+      0xf4,
+    ]);
 
-        sm.addConnection(relayClient);
-        // wait for manifest to be received
-        await promise;
-        const testAddr = Uint8Array.from([
-          0xf0,
-          0xf1,
-          0xf2,
-          0x03,
-          0x04,
-          0x05,
-          0xf6,
-          0xf7,
-          0xf8,
-          0x09,
-          0x0a,
-          0x0b,
-          0xfc,
-          0xfd,
-          0xfe,
-          0x0f,
-          0x01,
-          0x02,
-          0xf3,
-          0xf4,
-        ]);
+    const testCurrency = new Map<string, codec.CodecValue>([
+      ["Address", testAddr],
+      ["ChainID", 1337],
+    ]);
+    await sm.set(
+      ["Manifest", "PricingCurrency"],
+      testCurrency,
+    );
+    const value = await sm.get(["Manifest", "PricingCurrency"]);
+    assertEquals(
+      value,
+      new Map<string, codec.CodecValue>([
+        ["Address", testAddr],
+        ["ChainID", 1337],
+      ]),
+    );
+  });
 
-        const testCurrency = new Map<string, codec.CodecValue>([
-          ["Address", testAddr],
-          ["ChainID", 1337],
-        ]);
-        await sm.set(
-          ["Manifest", "PricingCurrency"],
-          testCurrency,
-        );
-        const value = await sm.get(["Manifest", "PricingCurrency"]);
-        assertEquals(
-          value,
-          new Map<string, codec.CodecValue>([
-            ["Address", testAddr],
-            ["ChainID", 1337],
-          ]),
-        );
-      });
-
-      await t.step(
-        "Make sure stateManager throws an error when a patch is rejected",
-        async () => {
-          const result = await assertRejects(() =>
-            sm.set(["Trash"], "Bad Value")
-          );
-          assertInstanceOf(result, ClientWriteError);
-        },
-      );
-      await t.step("Make sure we can do more then one bad write", async () => {
-        const result = await assertRejects(() =>
-          sm.set(["Trash"], "Another Bad Value")
-        );
-        assertInstanceOf(result, ClientWriteError);
-      });
-
-      await sm.close();
-
-      await t.step("Make sure we can close the state manager", async () => {
-        const pricingCurrencyPath = ["Manifest", "PricingCurrency"];
-        for (let i = 0; i < 2; i++) {
-          const nonce = relayClient.keyCardNonce;
-          const nsm = new StateManager({
-            store,
-            id: relayClient.shopId,
-          });
-          await nsm.open();
-          nsm.addConnection(relayClient);
-          assertEquals(
-            nonce,
-            relayClient.keyCardNonce,
-            "the new state manager should have loaded the nonce",
-          );
-          const oldValue = await nsm.get(pricingCurrencyPath);
-          await nsm.close();
-
-          // Test that we can set new values after reopening
-          const reopenedSm = new StateManager({
-            store,
-            id: relayClient.shopId,
-          });
-          await reopenedSm.open();
-          reopenedSm.addConnection(relayClient);
-
-          // Set a new value
-          const newTestValue = new Map<string, codec.CodecValue>([
-            ["Address", randomBytes(20)],
-            ["ChainID", 1337],
-          ]);
-          await reopenedSm.set(pricingCurrencyPath, newTestValue);
-
-          // wait until we see the new value from the relay
-          await new Promise((resolve) => {
-            reopenedSm.events.on((val) => {
-              if (equal(val, newTestValue)) {
-                resolve(void 0);
-              }
-            }, pricingCurrencyPath);
-          });
-
-          // Verify the value was set correctly
-          const retrievedValue = await reopenedSm.get(pricingCurrencyPath);
-          assertNotEquals(
-            retrievedValue,
-            oldValue,
-            `should not be the old value ${i}`,
-          );
-          assertEquals(
-            retrievedValue,
-            newTestValue,
-            `should be able to set and get values after reopening ${i}`,
-          );
-          await reopenedSm.close();
-        }
-      });
-
-      await relayClient.disconnect();
+  await t.step(
+    "Make sure stateManager throws an error when a patch is rejected",
+    async () => {
+      const result = await assertRejects(() => sm.set(["Trash"], "Bad Value"));
+      assertInstanceOf(result, ClientWriteError);
     },
-  },
-);
+  );
+  await t.step("Make sure we can do more then one bad write", async () => {
+    const result = await assertRejects(() =>
+      sm.set(["Trash"], "Another Bad Value")
+    );
+    assertInstanceOf(result, ClientWriteError);
+  });
+
+  await sm.close();
+
+  await t.step("Make sure we can close the state manager", async () => {
+    const pricingCurrencyPath = ["Manifest", "PricingCurrency"];
+    const nonce = relayClient.keyCardNonce;
+    const nsm = new StateManager({
+      store,
+      id: relayClient.shopId,
+    });
+    await nsm.open();
+    nsm.addConnection(relayClient);
+    assertEquals(
+      nonce,
+      relayClient.keyCardNonce,
+      "the new state manager should have loaded the nonce",
+    );
+    const oldValue = await nsm.get(pricingCurrencyPath);
+    await nsm.close();
+
+    // Test that we can set new values after reopening
+    const reopenedSm = new StateManager({
+      store,
+      id: relayClient.shopId,
+    });
+    await reopenedSm.open();
+    reopenedSm.addConnection(relayClient);
+
+    // Set a new value
+    const newTestValue = new Map<string, codec.CodecValue>([
+      ["Address", randomBytes(20)],
+      ["ChainID", 1337],
+    ]);
+    await reopenedSm.set(pricingCurrencyPath, newTestValue);
+
+    // wait until we see the new value from the relay
+    await new Promise((resolve) => {
+      reopenedSm.events.on((val) => {
+        if (equal(val, newTestValue)) {
+          resolve(void 0);
+        }
+      }, pricingCurrencyPath);
+    });
+
+    // Verify the value was set correctly
+    const retrievedValue = await reopenedSm.get(pricingCurrencyPath);
+    assertNotEquals(
+      retrievedValue,
+      oldValue,
+      `should not be the old value`,
+    );
+    assertEquals(
+      retrievedValue,
+      newTestValue,
+      `should be able to set and get values after reopening`,
+    );
+    await reopenedSm.close();
+  });
+
+  await relayClient.disconnect();
+});
