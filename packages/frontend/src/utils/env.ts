335f6903fa9f1166a51b33a6916600fc9aeeebd4
Alp <alp@mass.market>
2025-04-29 14:50:41 +0200

    chore(.env): implement solution to load .env files for dev/test/prod
    This was a tricky one.
    
    .env (aka dotenv) can be used to store configuration variables in a way
    that can live outside of a repo and vary depending on whether a project
    is being run in development mode (.env.development) or in production
    (.env.production).
    
    Vite's `import.meta.env` is supposed to contain the values of the
    corresponding env file (.env.development for dev). But in Deno, that
    doesn't work: `import.meta.env` can't be accessed.
    
    One work-around then is to load the .env files in Vite's config during
    startup and make them available on a variable that will contain the
    values thanks to Vite hooking into esbuild: https://esbuild.github.io/api/#define
    
    However, this behaves differently when running a development build with
    `deno run dev` and `deno build`, so that needs to be captured.
    
    Additionally, the testing environment does not go through Vite, and so
    the corresponding variables containing the .env values are not
    available. So these have to be read from disk and made available, and
    done so in a way that doesn't cause a build to fail because of fs not
    being available in the browser.
    
    Everything is compounded by the iteration speed of the current project,
    with multi-minute loads with Vite currently.
    ---
    
    Removed env-production.ts and env-vite.ts since these were workarounds
    for the .env problem.

--- packages/frontend/src/utils/env.ts
+++ packages/frontend/src/utils/env.ts
@@ -1,3 +1,6 @@
+import * as path from "jsr:@std/path";
+import parseDotenv from "./parse-dotenv.ts";
+
 export type EnvConfig = {
   chainName: string;
   relayEndpoint?: string;
@@ -8,8 +11,9 @@ export type EnvConfig = {
   matomoURL?: string;
 };
 
-import { envConfig as envVite } from "./env-vite.ts";
-import { envConfig as envProd } from "./env-production.ts";
+// Check if running in a HappyDOM environment (likely testing)
+export const isTesting = typeof globalThis !== "undefined" &&
+  !!(globalThis as unknown as { happyDOM: boolean }).happyDOM;
 
 let mode = "development";
 const viteEnv = (import.meta as unknown as { env: { MODE: string } }).env;
@@ -18,10 +22,62 @@ if (viteEnv && viteEnv.MODE) {
 } else if (typeof Deno !== "undefined" && Deno.env.get("MODE")) {
   mode = Deno.env.get("MODE")!;
 }
-const isProduction = mode === "production";
 
-export const env: EnvConfig = isProduction ? envProd : envVite;
+// TODO (@alp 2024-04-29): define more specific types
+type DotEnvFormat = {
+  VITE_CHAIN_NAME: string;
+  VITE_RELAY_ENDPOINT?: string;
+  VITE_RELAY_TOKEN_ID?: string;
+  VITE_SENTRY_DSN?: string;
+  VITE_ETH_RPC_URL?: string;
+  VITE_MATOMO_URL?: string;
+  VITE_SHOP_TOKEN_ID?: string;
+};
 
-// Check if running in a HappyDOM environment (likely testing)
-export const isTesting = typeof globalThis !== "undefined" &&
-  !!(globalThis as unknown as { happyDOM: boolean }).happyDOM;
+declare global {
+  let __ENV__: DotEnvFormat;
+}
+
+function getEnv(): EnvConfig {
+  let theEnv: DotEnvFormat;
+  if (isTesting) {
+    // loads .env from disk for testing. we do it this way because vite isn't running and we can't
+    // access .env through the mechanism used outside testing (see 'else' branch). this is possible because
+    // during testing we can access the file system
+
+    // NOTE (@alp 2025-04-29): `dotenvPath` is relative to the folder `env.ts` lives in. if that file, or its parent directory moves, this needs to be updated :)
+    const dirname = import.meta.dirname as string;
+    const dotenvPath = path.join(dirname, "..", "..");
+    const modeSuffix = (mode === "development" || mode === "production")
+      ? `.${mode}`
+      : "";
+    const modePath = path.join(dotenvPath, `.env${modeSuffix}`);
+    const text: string = Deno.readTextFileSync(modePath);
+    theEnv = parseDotenv(text);
+  } else {
+    // otherwise, get it from the esbuild's defined value __ENV__. during runtime and build, the value will contain the
+    // values from the correct .env.[development|production], depending on the runtime mode.
+
+    // when in browser && development, use Reflect to access __ENV__
+    if (mode === "development") {
+      theEnv = Reflect.get(globalThis, "__ENV__");
+    } else if (mode === "production") {
+      theEnv = __ENV__;
+    } else {
+      throw new Error(
+        `env.ts: should not hit this branch; mode was ${mode} which is not production nor development`,
+      );
+    }
+  }
+  return {
+    chainName: theEnv?.VITE_CHAIN_NAME,
+    ethRPCUrl: theEnv?.VITE_ETH_RPC_URL,
+    shopTokenId: theEnv?.VITE_SHOP_TOKEN_ID as `0x${string}`,
+    relayEndpoint: theEnv?.VITE_RELAY_ENDPOINT,
+    relayTokenId: theEnv?.VITE_RELAY_TOKEN_ID as `0x${string}`,
+    sentryDSN: theEnv?.VITE_SENTRY_DSN,
+    matomoURL: theEnv?.VITE_MATOMO_URL,
+  };
+}
+
+export const env: EnvConfig = getEnv();
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig, loadEnv, PluginOption } from "vite";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tailwindcss from "@tailwindcss/vite";
-
 // only needed for the build
 import { entryPoints } from "./scripts/generate-entry-points.ts";
 // Take all the routes and create an object with the route name as the key and the index.html as the value.
@@ -12,27 +11,34 @@ const buildInputs = Object.fromEntries(
   entryPoints.map((path) => [path, "index.html"]),
 );
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: buildInputs,
+export default defineConfig(({ mode }) => {
+  const dirname = import.meta.dirname as string;
+  const env = loadEnv(mode, dirname);
+  return {
+    build: {
+      rollupOptions: {
+        input: buildInputs,
+      },
+      outDir: "dist",
+      copyPublicDir: true,
     },
-    outDir: "dist",
-    copyPublicDir: true,
-    sourcemap: true,
-  },
-  plugins: [
-    deno() as PluginOption,
-    react(),
-    TanStackRouterVite({ addExtensions: true }),
-    nodePolyfills() as PluginOption,
-    tailwindcss() as PluginOption,
-  ],
-  optimizeDeps: {
-    include: [
-      // This prebundles the secp256k1 dependency which is used by some functions imported by viem (e.g. recoverPublicKey)
-      // Since the imported function uses dynamic import of this dependency. Dynamic import of modules seems to cause errors with vite.
-      "@noble/secp256k1",
+    define: {
+      __ENV__: JSON.stringify(env),
+      __MODE__: JSON.stringify(mode),
+    },
+    plugins: [
+      deno() as PluginOption,
+      react(),
+      TanStackRouterVite({ addExtensions: true }),
+      nodePolyfills() as PluginOption,
+      tailwindcss() as PluginOption,
     ],
-  },
+    optimizeDeps: {
+      include: [
+        // This prebundles the secp256k1 dependency which is used by some functions imported by viem (e.g. recoverPublicKey)
+        // Since the imported function uses dynamic import of this dependency. Dynamic import of modules seems to cause errors with vite.
+        "@noble/secp256k1",
+      ],
+    },
+  };
 });
