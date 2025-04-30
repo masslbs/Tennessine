16ce3ca7b70a40ef943d138bb3a98c7f9f41b7cc
Martin Becze <martin@becze.org>
2025-04-30 11:37:16 +0200

    fixed scripts

--- packages/frontend/scripts/build.ts
+++ packages/frontend/scripts/build.ts
@@ -1,7 +1,9 @@
 import { build } from "rolldown-vite";
 import config from "./vite.ts";
 
-await build(config);
+await build(
+  config({ mode: "production", command: "build" }),
+);
 
 // Vite spawns esbuild which prevents this from exiting automatically
 Deno.exit(0);
--- packages/frontend/scripts/dev.ts
+++ packages/frontend/scripts/dev.ts
@@ -1,6 +1,8 @@
 import { createServer } from "rolldown-vite";
 import config from "./vite.ts";
 
-const server = await createServer(config);
+const server = await createServer(
+  config({ mode: "development", command: "serve" }),
+);
 await server.listen();
 server.printUrls();
