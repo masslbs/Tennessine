4d12d377baa707ed3dfc7d9c3dd935606b3d6cfd
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-05-02 15:41:01 +0100

    updating merchant dashboard test to be more accurate

--- packages/frontend/src/components/merchants/MerchantDashboard_test.tsx
+++ packages/frontend/src/components/merchants/MerchantDashboard_test.tsx
@@ -7,46 +7,62 @@ import { random256BigInt } from "@massmarket/utils";
 import { allOrderListings, allOrders } from "@massmarket/schema/testFixtures";
 
 import MerchantDashboard from "./MerchantDashboard.tsx";
-import { createRouterWrapper } from "../../testutils/mod.tsx";
+import { createRouterWrapper, testClient } from "../../testutils/mod.tsx";
 
 Deno.test("Merchant Dashboard", {
   sanitizeResources: false,
   sanitizeOps: false,
-}, async (t) => {
+}, async () => {
   const shopId = random256BigInt();
-
-  await t.step("All orders render", async () => {
-    const {
-      wrapper,
-      stateManager,
-      relayClient,
-    } = await createRouterWrapper({
-      shopId,
-      createShop: true,
-      enrollMerchant: true,
-    });
-    stateManager.addConnection(relayClient);
-
-    for (const [key, entry] of allOrderListings.entries()) {
-      await stateManager.set(["Listings", key], entry);
-    }
-    let orderId: number;
-    for (const [key, entry] of allOrders.entries()) {
-      await stateManager.set(["Orders", key], entry);
-      orderId = key;
-    }
-
-    const { unmount } = render(<MerchantDashboard />, { wrapper });
-
-    await waitFor(async () => {
-      const orders = await screen.findAllByTestId("transaction");
-      expect(orders).toBeTruthy();
-      expect(orders.length).toBe(allOrders.size);
-      const order = screen.getAllByTestId(orderId!);
-      expect(order).toBeTruthy();
-      const status = screen.getAllByTestId("status");
-      expect(status[0].textContent).toBe("Open");
-    });
-    unmount();
+  // Setting up merchant
+  const {
+    wrapper: merchantWrapper,
+    stateManager: merchantStateManager,
+    relayClient: merchantRelayClient,
+  } = await createRouterWrapper({
+    shopId,
+    createShop: true,
+    enrollMerchant: true,
   });
+  merchantStateManager.addConnection(merchantRelayClient);
+
+  //Add listings to shop
+  for (const [key, entry] of allOrderListings.entries()) {
+    await merchantStateManager.set(["Listings", key], entry);
+  }
+
+  localStorage.removeItem(`keycard${shopId}`);
+
+  const { stateManager, relayClient, testAccount } = await createRouterWrapper({
+    shopId,
+  });
+
+  //Setting up customer
+  await relayClient.enrollKeycard(testClient, testAccount, true);
+  await relayClient.connect();
+  await relayClient.authenticate();
+  stateManager.addConnection(relayClient);
+  let orderId: number;
+
+  for (const [key, entry] of allOrders.entries()) {
+    await stateManager.set(["Orders", key], entry);
+    orderId = key;
+  }
+  localStorage.removeItem(`keycard${shopId}`);
+
+
+  const { unmount } = render(<MerchantDashboard />, {
+    wrapper: merchantWrapper,
+  });
+
+  await waitFor(async () => {
+    const orders = await screen.findAllByTestId("transaction");
+    expect(orders).toBeTruthy();
+    expect(orders.length).toBe(allOrders.size);
+    const order = screen.getAllByTestId(orderId!);
+    expect(order).toBeTruthy();
+    const status = screen.getAllByTestId("status");
+    expect(status[0].textContent).toBe("Open");
+  });
+  unmount();
 });
