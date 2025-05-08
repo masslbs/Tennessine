9b176e0fb13b43411435937b6a6a1cb41e5b90ae
Alp <alp@mass.market>
2025-05-08 11:42:28 +0200

    remove unused scripts

--- packages/frontend/scripts/dev.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from "rolldown-vite";
-import config from "./vite.ts";
-
-const server = await createServer(
-  config({ mode: "development", command: "serve" }),
-);
-await server.listen();
-server.printUrls();
