0a1d56184c3a15a7c8968f9cc348acf494a570c3
Alp <alp@mass.market>
2025-05-08 11:30:01 +0200

    fix(vite.config.ts): remove commented out plugin

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv, PluginOption } from "vite";
 import deno from "@deno/vite-plugin";
-// import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.1";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
