7edf2aa8b7ee73f2b15572b61cf5b9cc13ebd864
Al <alp@mass.market>
2025-05-08 12:24:09 +0200

    Revert vite pipeline changes (#468)
    * revert: revert changes to frontend pipeline
    
    Reverts parts of 50403f9071c5f6715240fcf3fcdb713f274dbae3
    
    The reason is the frontend on my machine would not successfully execute
    with the reverted changes in place; the shop ui would never load.
    
    In order to progress on #436, I needed to have a working baseline from
    which to start making changes.
    
    * gitignore: add cache.json for deno-vite-plugin fix
    
    see https://github.com/masslbs/Tennessine/issues/436#issuecomment-2861969601
    
    the location may change in the future, but it's currently relative to
    where deno run dev / deno run build is executed from
    
    * fix(vite.config.ts): remove commented out plugin
    
    * remove unused scripts
    
    * fix(frontend/deno.json): re-add imports missed due to hand-revert
    
    I missed some packages that were changed in the deno.json due to
    hand-reverting parts of 50403f9

--- packages/frontend/scripts/build.ts
+++
@@ -1,9 +0,0 @@
-import { build } from "rolldown-vite";
-import config from "./vite.ts";
-
-await build(
-  config({ mode: "production", command: "build" }),
-);
-
-// Vite spawns esbuild which prevents this from exiting automatically
-Deno.exit(0);
