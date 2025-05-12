2e86ae684bf239b503af6084d6b40950a9df5668
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-05-12 15:58:59 +0100

    load motamo if cookieConsent = "accepted"

--- packages/frontend/src/components/CookieBanner.tsx
+++ packages/frontend/src/components/CookieBanner.tsx
@@ -2,26 +2,30 @@ import { useEffect, useState } from "react";
 import { CookieConsent } from "../types.ts";
 import { logger } from "@massmarket/utils";
 
-const namespace = "frontend: CookieBanner";
+const namespace = "frontend:CookieBanner";
 const debug = logger(namespace, "debug");
 const errlog = logger(namespace, "error");
 
 const CookieBanner = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
   const [consent, setConsent] = useState<CookieConsent | null>(null);
+  const sessionConsent = sessionStorage.getItem("cookieConsent");
 
   useEffect(() => {
-    const sessionConsent = sessionStorage.getItem("cookieConsent");
     if (sessionConsent) {
       setConsent(sessionConsent as CookieConsent);
-      setIsVisible(false);
+      if (sessionConsent === "accepted") {
+        import("../matomo.js").catch((e) => {
+          errlog(`failed to load matomo ${e}`);
+        });
+      }
+    } else {
+      // Only show cookie banner if cookie consent is not found in session storage.
+      setIsVisible(true);
     }
-  }, []);
+  }, [sessionConsent]);
 
   const handleAccept = () => {
-    import("../matomo.js").catch((e) => {
-      errlog(`failed to load matomo ${e}`);
-    });
     setConsent("accepted");
     setIsVisible(false);
     sessionStorage.setItem("cookieConsent", "accepted");
@@ -40,7 +44,7 @@ const CookieBanner = () => {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-400 text-white p-4">
+    <div className="fixed bottom-0 left-0 right-0 bg-zinc-700 text-white p-4">
       <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
         <div className="flex-1">
           <h3 className="text-lg font-bold mb-2">Cookie Notice</h3>
@@ -60,14 +64,14 @@ const CookieBanner = () => {
         <div className="flex md:flex-col gap-3 mt-3 md:mt-0">
           <button
             onClick={handleAccept}
-            className="bg-[#3b513e] px-6 py-2 rounded-md"
+            className="bg-[#3b513e] px-6 py-2 rounded-md cursor-pointer"
             type="button"
           >
             Accept All
           </button>
           <button
             onClick={handleReject}
-            className="bg-gray-500 px-6 py-2 rounded-md"
+            className="bg-gray-500 px-6 py-2 rounded-md cursor-pointer"
             type="button"
           >
             Reject All
