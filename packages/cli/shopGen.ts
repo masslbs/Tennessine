e3649b430526f5cb1632ce0daaf7c1e6648249f6
wanderer <martin@becze.org>
2025-05-15 19:58:41 +0200

    Shop Generation Script (#506)
    * Added script to generate a random show
    
    * Added a simple shop generator script
    
    * Add readme

---
+++ packages/cli/shopGen.ts
@@ -0,0 +1,58 @@
+import { numberToHex } from "viem";
+import { faker } from "@faker-js/faker";
+import {
+  createTestBlockchainClient,
+  createTestRelayClient,
+} from "@massmarket/client/test";
+import { MemStore } from "@massmarket/store/mem";
+import { Listing, ListingMetadata } from "@massmarket/schema";
+import StateManager from "@massmarket/stateManager";
+
+const blockchainClient = createTestBlockchainClient();
+const relayClient = await createTestRelayClient(blockchainClient);
+
+console.log("creating shop", numberToHex(relayClient.shopId));
+
+const root = new Map(Object.entries({
+  Tags: new Map(),
+  Orders: new Map(),
+  Accounts: new Map(),
+  Inventory: new Map(),
+  Listings: new Map(),
+  Manifest: new Map(),
+  SchemeVersion: 1,
+}));
+
+const store = new MemStore();
+const sm = new StateManager({
+  store,
+  id: relayClient.shopId,
+  defaultState: root,
+});
+
+await sm.open();
+
+sm.addConnection(relayClient);
+
+const writes = [];
+for (let i = 1; i < 100; i++) {
+  writes.push(
+    sm.set(
+      ["Listings", i],
+      new Listing(
+        i,
+        BigInt(faker.commerce.price({ dec: 0 })),
+        new ListingMetadata(
+          faker.commerce.productName(),
+          faker.commerce.productDescription(),
+          [faker.image.urlPicsumPhotos()],
+        ),
+        1,
+      ),
+    ),
+  );
+}
+
+await Promise.all(writes);
+
+sm.close();
