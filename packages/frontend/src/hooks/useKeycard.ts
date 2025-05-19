ed0fb291e9247f36150cad3f3ba6aaef7fd893fb
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-05-19 22:51:33 +0100

    use persister with localStorage & setQueriesData

--- packages/frontend/src/hooks/useKeycard.ts
+++ packages/frontend/src/hooks/useKeycard.ts
@@ -1,9 +1,7 @@
-import { assert } from "@std/assert";
-import { useLocalStorage } from "@uidotdev/usehooks";
+import { useAccount, useWalletClient } from "wagmi";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import { getLogger } from "@logtape/logtape";
-import { useWalletClient } from "wagmi";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { RelayClient } from "@massmarket/client";
 import { getWindowLocation } from "@massmarket/utils";
@@ -11,75 +9,63 @@ import { getWindowLocation } from "@massmarket/utils";
 import { useShopId } from "./useShopId.ts";
 import { KeycardRole } from "../types.ts";
 import { useRelayEndpoint } from "./useRelayEndpoint.ts";
-
-type Keycard = {
-  privateKey: `0x${string}`;
-  role: KeycardRole;
-  address: `0x${string}`;
-} | null;
+import { queryClient } from "../App.tsx";
 
 const logger = getLogger(["mass-market", "frontend", "useKeycard"]);
 
 export function useKeycard(role: KeycardRole = "guest") {
   const { shopId } = useShopId();
   const { data: wallet } = useWalletClient();
+  const account = useAccount();
   const { relayEndpoint } = useRelayEndpoint();
-  const [keycard, setKeycard] = useLocalStorage<Keycard>(
-    "keycard" + shopId,
-    null,
-  );
+  const queryClient = useQueryClient();
 
-  const { isPending, data } = useQuery({
-    queryKey: ["keycard", shopId?.toString(), relayEndpoint, wallet, role],
+  return useQuery({
+    queryKey: [
+      "keycard",
+      role,
+    ],
     queryFn: async () => {
       // 1. Generate KC
       // 2. Enroll KC
       // 3. Save to localStorage
-
-      // This is for instances when the user has a guest keycard and wants to log in as a merchant.
-      const guestToMerchant = role === "merchant" && keycard?.role === "guest";
-      if (keycard && !guestToMerchant) {
-        return keycard;
-      }
-      if (!shopId || !relayEndpoint || !wallet) {
-        assert(shopId, "shopId is required");
-        assert(relayEndpoint, "relayEndpoint is required");
-        assert(wallet, "wallet is required");
-      }
-
       const privateKey = generatePrivateKey();
-      const account = privateKeyToAccount(privateKey);
 
       // This relay instance is just to enroll the keycard.
       const relayClient = new RelayClient({
         relayEndpoint,
         walletClient: wallet,
-        keycard: account,
+        keycard: privateKeyToAccount(privateKey),
         shopId,
       });
 
       const res = await relayClient.enrollKeycard(
         wallet,
         account,
-        true,
+        role === "guest",
         getWindowLocation(),
       );
+
       if (!res.ok) {
         throw new Error(`Failed to enroll keycard: ${res}`);
       }
+      logger.debug(`Success: Enrolled new ${role} keycard`);
+
       const kc = {
         privateKey,
         role,
         address: account!.address,
       };
-      setKeycard(kc);
-      logger.debug("Success: Enrolled new guest keycard");
+      if (role === "merchant") {
+        // Return this keycard for all keycard queries.
+        queryClient.setQueriesData(
+          { queryKey: ["keycard"] },
+          kc,
+        );
+      }
+
       return kc;
     },
+    enabled: !!shopId && !!wallet && !!relayEndpoint && !!account,
   });
-
-  return {
-    data,
-    isPending,
-  };
 }
--- packages/frontend/src/hooks/useKeycard_test.tsx
+++ packages/frontend/src/hooks/useKeycard_test.tsx
@@ -27,23 +27,17 @@ Deno.test("useKeycard", {
     const { result } = renderHook(() => useKeycard(), { wrapper });
     await waitFor(() => {
       expect(result.current.data?.role).toEqual("guest");
-      pk = result.current.data?.privateKey as string;
+      pk = result.current.data?.privateKey as `0x${string}`;
     });
   });
 
-  await t.step("should return cached keycard", async () => {
-    const { result } = renderHook(() => useKeycard(), { wrapper });
-    await waitFor(() => {
-      expect(result.current.data?.role).toEqual("guest");
-      expect(result.current.data?.privateKey).toEqual(pk);
+  await t.step("should enroll merchant keycard", async () => {
+    const { result } = renderHook(() => useKeycard("merchant"), {
+      wrapper,
     });
-  });
-
-  await t.step("should swap guest keycard to a merchant keycard", async () => {
-    const { result } = renderHook(() => useKeycard("merchant"), { wrapper });
     await waitFor(() => {
       expect(result.current.data?.role).toEqual("merchant");
-      expect(result.current.data?.privateKey).not.toEqual(pk);
+      pk = result.current.data?.privateKey as `0x${string}`;
     });
   });
   await t.step(
@@ -61,17 +55,23 @@ Deno.test("useKeycard", {
   await t.step(
     "if useKeycard is called concurrently with different roles, merchant keycard should be returned",
     async () => {
-      const { unmount } = render(<TestComponent role="merchant" />, {
+      const { unmount, rerender } = render(<TestComponent role="merchant" />, {
         wrapper,
       });
       await waitFor(() => {
+        const component1Role = screen.getByTestId("component1-role");
+        const component2Role = screen.getByTestId("component2-role");
+        expect(component1Role.textContent).toEqual("merchant");
+        expect(component2Role.textContent).toEqual("merchant");
         const component1Pk = screen.getByTestId("component1-pk");
         const component2Pk = screen.getByTestId("component2-pk");
         expect(component1Pk.textContent).toEqual(component2Pk.textContent);
       });
+
       unmount();
     },
   );
+
   cleanup();
 });
 
@@ -85,9 +85,19 @@ const TestComponent = ({ role }: { role: KeycardRole }) => {
 };
 const ChildComponent = () => {
   const { data } = useKeycard();
-  return <div data-testid="component1-pk">{data?.privateKey}</div>;
+  return (
+    <div>
+      <p data-testid="component1-pk">{data?.privateKey}</p>
+      <p data-testid="component1-role">{data?.role}</p>
+    </div>
+  );
 };
 const ChildComponent2 = ({ role }: { role: KeycardRole }) => {
   const { data } = useKeycard(role);
-  return <div data-testid="component2-pk">{data?.privateKey}</div>;
+  return (
+    <div>
+      <p data-testid="component2-pk">{data?.privateKey}</p>
+      <p data-testid="component2-role">{data?.role}</p>
+    </div>
+  );
 };
