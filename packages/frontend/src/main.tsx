0f6fec3782153565d5c74f116b4e22bcbb52d02e
Henry <111202+cryptix@users.noreply.github.com>
2025-05-22 13:01:47 +0200

    fix error reporting on staging builds (#536)
    fixes #535

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,35 +1,39 @@
 import "web-streams-polyfill/polyfill";
 import { init } from "@sentry/browser";
-import { configure, getConsoleSink } from "@logtape/logtape";
+import { Config, configure, getConsoleSink } from "@logtape/logtape";
 import { getSentrySink } from "@logtape/sentry";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { env } from "./utils/env.ts";
 import releaseInfo from "./release-info.json" with { type: "json" };
-const isProd = env.chainName === "mainnet";
 
-let sentryClient;
-if (env.sentryDSN) {
-  sentryClient = init({
+const isSentryEnabled = env.sentryDSN !== undefined;
+
+const sentryConfig: Config<string, string> = {
+  sinks: {
+    console: getConsoleSink(),
+  },
+  loggers: [
+    { category: [], sinks: ["console"], level: "debug" },
+  ],
+};
+if (isSentryEnabled) {
+  const isProd = env.chainName === "mainnet";
+  const sentryClient = init({
     dsn: env.sentryDSN,
     environment: isProd ? "production" : "development",
     release: `tennessine@${releaseInfo.version}`, // TODO: we should start tagging our builds...
     tracesSampleRate: isProd ? 0.1 : 1.0,
   });
+  sentryConfig.sinks.sentry = getSentrySink(sentryClient as undefined);
+  sentryConfig.loggers.push({
+    category: ["mass-market"],
+    sinks: ["sentry"],
+    level: "warning",
+  });
 }
-
-await configure({
-  sinks: {
-    sentry: getSentrySink(sentryClient as undefined),
-    console: getConsoleSink(),
-  },
-  loggers: [
-    isProd
-      ? { category: ["mass-market"], sinks: ["sentry"], level: "warning" }
-      : { category: [], sinks: ["console"], level: "debug" },
-  ],
-});
+await configure(sentryConfig);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
