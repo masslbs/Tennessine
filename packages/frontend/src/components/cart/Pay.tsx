78cc266905b389f740de17713ecfac85c54c3256
Alp <alp@mass.market>
2025-05-26 16:13:46 +0200

    test(pay): for errors - surface nicer messages for the user and error details for the logs
    progresses #537

--- packages/frontend/src/components/cart/Pay.tsx
+++ packages/frontend/src/components/cart/Pay.tsx
@@ -143,12 +143,16 @@ export default function Pay({
         throw new Error("pay: transaction failed");
       }
     } catch (error: unknown) {
+      // for context on typing errors from wagmi, see
+      // https://github.com/wevm/wagmi/discussions/233
+      setErrorMsg("Error sending payment");
       // @ts-ignore TODO: fix this
       if (error.shortMessage) {
         // @ts-ignore TODO: fix this
-        setErrorMsg(error.shortMessage);
+        logger.info`${error.shortMessage}`;
       }
-      logger.error("Error sending payment", { error });
+      // @ts-ignore TODO: fix this
+      logger.error("Error sending payment {error}", { error });
     }
   }
 
