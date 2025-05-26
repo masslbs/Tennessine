666a4aebdf47a98a1171cf5a0f9cb70b35da6474
Martin Becze <martin@becze.org>
2025-05-26 12:10:17 +0200

    use tanstack query for relay endpoint info

--- packages/frontend/src/hooks/useRelayEndpoint.ts
+++ packages/frontend/src/hooks/useRelayEndpoint.ts
@@ -1,29 +1,36 @@
 import { getLogger } from "@logtape/logtape";
 import { discoverRelay } from "@massmarket/client";
+import { useQuery } from "@tanstack/react-query";
 import { env } from "../utils/env.ts";
-import { useQuery } from "../hooks/useQuery.ts";
 
 const logger = getLogger(["mass-market", "frontend", "useRelayEndpoint"]);
 
 export function useRelayEndpoint() {
-  const { result } = useQuery(async () => {
-    if (env.relayTokenId && env.relayEndpoint) {
-      const re = {
-        url: new URL(env.relayEndpoint),
-        tokenId: env.relayTokenId,
-      };
-      logger.debug(`using environment variables for relay endpoint ${re.url}`);
-      return re;
-    } else {
-      const discovered = await discoverRelay("ws://localhost:4444/v4");
-      if (!discovered.url) throw new Error("Relay endpoint URL not set");
-      if (!discovered.tokenId) {
-        throw new Error("Relay endpoint tokenId not set");
-      }
-      logger.debug(`using testing relay endpoint ${discovered.url}`);
-      return discovered;
-    }
-  });
+  const q = useQuery(
+    {
+      queryKey: ["relayEndpoint"],
+      queryFn: async () => {
+        if (env.relayTokenId && env.relayEndpoint) {
+          const re = {
+            url: new URL(env.relayEndpoint),
+            tokenId: env.relayTokenId,
+          };
+          logger.debug(
+            `using environment variables for relay endpoint ${re.url}`,
+          );
+          return re;
+        } else {
+          const discovered = await discoverRelay("ws://localhost:4444/v4");
+          if (!discovered.url) throw new Error("Relay endpoint URL not set");
+          if (!discovered.tokenId) {
+            throw new Error("Relay endpoint tokenId not set");
+          }
+          logger.debug(`using testing relay endpoint ${discovered.url}`);
+          return discovered;
+        }
+      },
+    },
+  );
 
-  return { relayEndpoint: result };
+  return { relayEndpoint: q.data, ...q };
 }
