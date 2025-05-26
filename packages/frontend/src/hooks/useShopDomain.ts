1556bd9f96b8696d02623febfcebaed94b4137f0
Martin Becze <martin@becze.org>
2025-05-26 12:25:42 +0200

    remove uneeded hooks

--- packages/frontend/src/hooks/useShopDomain.ts
+++
@@ -1,6 +0,0 @@
-export function useShopDomain(): { protocol: string; shopDomain: string } {
-  return {
-    protocol: globalThis.location.protocol,
-    shopDomain: globalThis.location.host,
-  };
-}
