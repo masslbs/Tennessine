b6f5e1f8c92b7f66dddbd11d71d00a8f118c2711
wanderer <martin@becze.org>
2025-05-26 12:34:47 +0200

    use tanstack query for relay endpoint info (#549)
    * use tanstack query for relay endpoint info
    * remove uneeded hooks

--- packages/frontend/src/hooks/usePathname.ts
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "@tanstack/react-router";
-
-export function usePathname() {
-  const router = useRouter();
-  const isMerchantPath = ["/create-shop", "/merchant-connect"].includes(
-    router?.state?.location?.pathname,
-  );
-  return { pathname: router?.state?.location?.pathname, isMerchantPath };
-}
--- packages/frontend/src/hooks/useRelayEndpoint_test.ts
+++ packages/frontend/src/hooks/useRelayEndpoint_test.ts
@@ -1,19 +1,22 @@
 import "../happyDomSetup.ts";
 import { assertEquals } from "@std/assert";
-import { cleanup, renderHook, waitFor } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useRelayEndpoint } from "./useRelayEndpoint.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-Deno.test("useRelayEndpoint", {
-  sanitizeResources: false,
-  sanitizeOps: false,
-}, async (t) => {
+const queryClient = new QueryClient();
+
+function wrapper({ children }: { children: React.ReactNode }) {
+  return QueryClientProvider({ client: queryClient, children });
+}
+
+Deno.test("useRelayEndpoint", async (t) => {
   await t.step("If no env vars, call discoverRelay fn", async () => {
-    const { result, unmount } = renderHook(() => useRelayEndpoint());
+    const { result } = renderHook(() => useRelayEndpoint(), {
+      wrapper,
+    });
     await waitFor(() => {
       assertEquals(!!result.current.relayEndpoint, true);
     });
-    unmount();
   });
-
-  cleanup();
 });
