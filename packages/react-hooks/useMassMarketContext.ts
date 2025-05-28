8fc6b151042458097b1bed59ec60f126723b6241
Martin Becze <martin@becze.org>
2025-05-28 13:51:36 +0200

    don't need to throw if there is no context

--- packages/react-hooks/useMassMarketContext.ts
+++ packages/react-hooks/useMassMarketContext.ts
@@ -2,11 +2,5 @@ import { useContext } from "react";
 import { MassMarketContext } from "./MassMarketContext.ts";
 
 export function useMassMarketContext() {
-  const context = useContext(MassMarketContext);
-  if (!context) {
-    throw new Error(
-      "useMassMarketContext must be used within a MassMarketProvider",
-    );
-  }
-  return context;
+  return useContext(MassMarketContext);
 }
