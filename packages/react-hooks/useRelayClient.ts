44037ef6a94867dd311ecfb6e2ba60200a909da5
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-05-29 16:54:29 +0100

    documentation

--- packages/react-hooks/useRelayClient.ts
+++ packages/react-hooks/useRelayClient.ts
@@ -11,6 +11,12 @@ import { useShopId } from "./useShopId.ts";
 
 const logger = getLogger(["mass-market", "frontend", "useRelayClient"]);
 
+/**
+ * This hook instantiates a RelayClient class and connects and authenticates the relay.
+ * This query will run on every app session.
+ * Since useQuery caches data in memory during a single app session, it will return the RelayClient class.
+ * However, during refreshes (or when the app unmounts), data is cached in the configured persister (localStorage), and since the class cannot be serialized the query is not cached.
+ */
 export function useRelayClient() {
   const { data: keycard } = useKeycard();
   const { relayEndpoint } = useRelayEndpoint();
@@ -32,6 +38,7 @@ export function useRelayClient() {
       logger.debug`RelayClient connected and authenticated.`;
       return rc;
     },
+    // This ensures that the query will only run once during the app session.
     gcTime: Infinity,
     staleTime: Infinity,
   });
