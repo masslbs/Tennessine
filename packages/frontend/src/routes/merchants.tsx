6497bf87802b31be963b1d9d41418ee02ba00a5a
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2025-08-27 09:50:02 +0200

    documentation for merchants redirect

--- packages/frontend/src/routes/merchants.tsx
+++ packages/frontend/src/routes/merchants.tsx
@@ -6,9 +6,14 @@ export const Route = createFileRoute("/merchants")({
   component: RouteComponent,
 });
 
+/**
+ * This ensures that if the current keycard is not of role type merchant and the user is on any /merchants screen, they are redirected to the root route.
+ * This is for cases when merchants disconnect their wallet account while on a /merchants screen.
+ */
 function RouteComponent() {
   const navigate = useNavigate();
   const { keycard } = useKeycard();
+
   if (keycard && keycard.role !== "merchant") {
     navigate({ to: "/" });
   }
